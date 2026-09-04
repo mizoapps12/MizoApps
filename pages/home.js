b3913a6b6c9a41ca90cd27dc277574b2489f5484
mizoapps12 <mizoapps12@gmail.com>
2026-09-04 16:37:32 +0530

    Update home.js

--- pages/home.js
+++ pages/home.js
@@ -1,5 +1,5 @@
 
-import Header from '../components/Header';
+import Header from '@/components/Header';
 import { useEffect, useState } from 'react';
 import { collection, addDoc, onSnapshot, query, orderBy, doc, updateDoc, arrayUnion, arrayRemove } from 'firebase/firestore';
 import { db, auth } from '@/firebase';
