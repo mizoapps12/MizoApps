6ce219c971894664bf8354da23a626682bf5f1e5
mizoapps12 <mizoapps12@gmail.com>
2026-09-04 21:08:09 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,18 @@
 import './globals.css'
-export const metadata = { title: 'MizoApps - Mizo Thawnthu', description: 'Mizo thawnthu tha ber' }
+import Header from './components/Header'
+import { SettingsProvider } from './components/SettingsContext'
+
+export const metadata = { title: 'MizoApps', description: 'Mizo thawnthu tha ber' }
+
 export default function RootLayout({ children }) {
-  return (<html lang="en"><body>{children}</body></html>)
+  return (
+    <html lang="en">
+      <body>
+        <SettingsProvider>
+          <Header/>
+          {children}
+        </SettingsProvider>
+      </body>
+    </html>
+  )
 }
