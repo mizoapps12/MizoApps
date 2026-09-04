3bbe353fdd00fc10be47312735e762411a5d8734
mizoapps12 <mizoapps12@gmail.com>
2026-09-04 21:17:30 +0530

    Create page.js

---
+++ app/contact/page.js
@@ -0,0 +1,16 @@
+export default function Contact(){
+  return(
+    <div className="container">
+      <h2>📩 Contact Us</h2>
+      <div className="card">
+        <p>Thawnthu thar rawn thawh i duh em? Harsatna i nei em?</p>
+        <br/>
+        <p>📧 Email: mizoapps@gmail.com</p>
+        <p>📱 WhatsApp: +91 12345 67890</p>
+        <p>🌐 Website: mizo-apps.vercel.app</p>
+        <br/>
+        <p>Mizo tawng tihhmasawn nan kan thawk dun zel ang!</p>
+      </div>
+    </div>
+  )
+    }
