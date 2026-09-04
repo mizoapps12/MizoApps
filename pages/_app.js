692954de259f5194a2385a26c130c0bc98dd0e42
mizoapps12 <mizoapps12@gmail.com>
2026-09-04 14:21:00 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,2 @@
-import '../styles/globals.css'
-
-export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+import '../styles/globals.css';
+export default function App({Component, pageProps}){ return <Component {...pageProps} /> }
