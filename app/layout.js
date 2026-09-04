40b14803cd66cd11f464052a9a8d9089a535b44c
mizoapps12 <mizoapps12@gmail.com>
2026-09-04 20:36:17 +0530

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,5 @@
+import './globals.css'
+export const metadata = { title: 'MizoApps - Mizo Thawnthu', description: 'Mizo thawnthu tha ber' }
+export default function RootLayout({ children }) {
+  return (<html lang="en"><body>{children}</body></html>)
+}
