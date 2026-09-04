fb85682a861ee16587152c64b059fe14725d0d7e
mizoapps12 <mizoapps12@gmail.com>
2026-09-04 21:16:56 +0530

    Create page.js

---
+++ app/privacy/page.js
@@ -0,0 +1,16 @@
+export default function Privacy(){
+  return(
+    <div className="container">
+      <h2>🔒 Privacy Policy</h2>
+      <div className="card">
+        <p><b>MizoApps</b> chuan i data kan zah sak a ni.</p>
+        <p>1. I thawnthu chhiar duh zawng kan save lo.</p>
+        <p>2. Email leh password Admin tan chauh a ni.</p>
+        <p>3. Ads emaw tracking emaw kan hmang lo.</p>
+        <p>4. I chhiar tawh thawnthu te i phone ah chauh a in save.</p>
+        <br/>
+        <p>Contact: mizoapps@gmail.com</p>
+      </div>
+    </div>
+  )
+}
