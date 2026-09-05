ec6528d1d09f0e2ce524839f997af191608c404c
mizoapps12 <mizoapps12@gmail.com>
2026-09-05 17:55:18 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -120,7 +120,7 @@ export default function HomePage(){
                   {preview}...
                 </span>
                 {/* DING LAM AH DAH */}
-                <div style={{textAlign:'right', marginTop:'7px'}}>
+                <div style={{textAlign:'right', marginTop:'5px'}}>
                   <span onClick={()=>handleStoryClick(story.id)} style={{color:'#16a34a', fontWeight:'800', cursor:'pointer', fontSize:'14px'}}>
                     Read more...
                   </span>
